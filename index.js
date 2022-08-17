dee836f0efc88147ae270253361f4ddd3422e7e8
choi jong yun <zy1234615@gmail.com>
2022-08-17 17:17:41 +0900

    end

--- index.js
+++ index.js
@@ -39,31 +39,31 @@ app.get("/news/:bookname", (req, res) => {
   axios({
     url: `https://openapi.naver.com/v1/search/news.json?query=${queryTxt}&display=20`,
     headers: {
-      "X-Naver-Client-Id": "775S2Y6x8tZKZ77mA7e_",
-      "X-Naver-Client-Secret": "RpyHT_zjC4",
+      "X-Naver-Client-Id": NAVER_ID,
+      "X-Naver-Client-Secret": NAVER_SECRET_ID,
     },
   }).then(function (response) {
     res.json(response.data);
   });
 });
-app.get("/news02", (req, res) => {
-  // /:~~~ 주소가아닌 넘어가는데이터값 입력식임,변수로받겠다는신호(params)news까지가주소
-  const queryTxt = encodeURIComponent(req.query.bookname);
-  // query방식은 주소뒤에 ?key값=value값로 입력 ex)?query=폭우
-  axios({
-    url: `https://openapi.naver.com/v1/search/news.json?query=${queryTxt}`,
-    headers: {
-      "X-Naver-Client-Id": "775S2Y6x8tZKZ77mA7e_",
-      "X-Naver-Client-Secret": "RpyHT_zjC4",
-    },
-    data: {
-      display: 5,
-    },
-  }).then(function (response) {
-    console.log(response.data);
-    res.json(response.data);
-  });
-});
+// app.get("/news02", (req, res) => {
+//   // /:~~~ 주소가아닌 넘어가는데이터값 입력식임,변수로받겠다는신호(params)news까지가주소
+//   const queryTxt = encodeURIComponent(req.query.bookname);
+//   // query방식은 주소뒤에 ?key값=value값로 입력 ex)?query=폭우
+//   axios({
+//     url: `https://openapi.naver.com/v1/search/news.json?query=${queryTxt}`,
+//     headers: {
+//       "X-Naver-Client-Id": "775S2Y6x8tZKZ77mA7e_",
+//       "X-Naver-Client-Secret": "RpyHT_zjC4",
+//     },
+//     data: {
+//       display: 5,
+//     },
+//   }).then(function (response) {
+//     console.log(response.data);
+//     res.json(response.data);
+//   });
+// });
 //---------------------------------------------------
 app.get("/movie/:moviename", (req, res) => {
   const queryTxt = encodeURIComponent(req.params.moviename);
@@ -72,27 +72,27 @@ app.get("/movie/:moviename", (req, res) => {
   axios({
     url: `https://openapi.naver.com/v1/search/movie.json?query=${queryTxt}`,
     headers: {
-      "X-Naver-Client-Id": "775S2Y6x8tZKZ77mA7e_",
-      "X-Naver-Client-Secret": "RpyHT_zjC4",
+      "X-Naver-Client-Id": NAVER_ID,
+      "X-Naver-Client-Secret": NAVER_SECRET_ID,
     },
   }).then(function (response) {
     res.json(response.data);
     // console.log(response.data);
   });
 });
-app.get("/book02", (req, res) => {
-  const queryTxt = encodeURIComponent(req.query.bookname);
-  // console.log(req.params.bookname);
-  axios({
-    url: `https://openapi.naver.com/v1/search/book.json?query=${queryTxt}&display=100`,
-    headers: {
-      "X-Naver-Client-Id": "775S2Y6x8tZKZ77mA7e_",
-      "X-Naver-Client-Secret": "RpyHT_zjC4",
-    },
-  }).then(function (response) {
-    res.json(response.data);
-  });
-});
+// app.get("/book02", (req, res) => {
+//   const queryTxt = encodeURIComponent(req.query.bookname);
+//   // console.log(req.params.bookname);
+//   axios({
+//     url: `https://openapi.naver.com/v1/search/book.json?query=${queryTxt}&display=100`,
+//     headers: {
+//       "X-Naver-Client-Id": "775S2Y6x8tZKZ77mA7e_",
+//       "X-Naver-Client-Secret": "RpyHT_zjC4",
+//     },
+//   }).then(function (response) {
+//     res.json(response.data);
+//   });
+// });
 
 app.get("/login", (req, res) => {
   console.log(req.query);
